f4c126c77fa3aa19ee10ba623afec88d34637a9b
Harsh <kumarharsh93982@gmail.com>
2026-03-19 10:05:12 +0530

    added gaurd to the prisma client.

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,29 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "@prisma/client";
+import envConfig from "../config/env.config.js";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+// Instantiate directly
+const prisma =
+  globalThis.prisma ||
+  (() => {
+    if (!envConfig.DATABASE_URL) {
+      throw new Error(
+        "DATABASE_URL is missing. Set it in your environment before starting the server.",
+      );
+    }
+
+    const adapter = new PrismaMariaDb(envConfig.DATABASE_URL);
+
+    return new PrismaClient({
+      adapter,
+      log: ["query", "error", "warn"],
+    });
+  })();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
+export default prisma;
