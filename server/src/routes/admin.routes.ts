d24af03225c77dbc7844d6413450588975a11c24
Harsh <kumarharsh93982@gmail.com>
2026-03-20 22:30:55 +0530

    all createuser, getuser, deleteuser routes here.

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -1,10 +1,35 @@
 import express from "express";
-import { createUser } from "../controllers/admin.controller.js";
+import {
+  allUsers,
+  createUser,
+  deleteUser,
+  UserById,
+} from "../controllers/admin.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import {authorizeRoles}  from "../middlewares/roleBased.middleware.js";
+import { authorizeRoles } from "../middlewares/roleBased.middleware.js";
+import { canCreateUser } from "../middlewares/create.middleware.js";
+import { canDeleteUser } from "../middlewares/delete.middleware.js";
 
 const router = express.Router();
 
-router.post("/create-user", authMiddleware, authorizeRoles("ADMIN"), createUser);
+router.get("/users", authMiddleware, authorizeRoles("ADMIN"), allUsers);
+
+router.post(
+  "/users",
+  authMiddleware,
+  authorizeRoles("ADMIN"),
+  canCreateUser(),
+  createUser,
+);
+
+router.delete(
+  "/users/:id",
+  authMiddleware,
+  authorizeRoles("ADMIN"),
+  canDeleteUser(),
+  deleteUser,
+);
+
+router.get("/users/:id", authMiddleware, authorizeRoles("ADMIN"), canCreateUser(), UserById);
 
 export default router;
